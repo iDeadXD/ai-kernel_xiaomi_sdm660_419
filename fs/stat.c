8c65c4bb54a2e7fef7edb8b12a74b1c8c63aa889
iDead XD <reinforced113@gmail.com>
2025-03-10 23:03:01 +0000

    KernelSU: Don't run ksu command when KernelSU is disabled

--- fs/stat.c
+++ fs/stat.c
@@ -21,6 +21,10 @@
 #include <linux/susfs_def.h>
 #endif
 
+#ifdef CONFIG_KSU
+#include <linux/ksu.h>
+#endif
+
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
@@ -383,7 +387,8 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	int error;
 
 #ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flag);
+	if (get_ksu_state() > 0)
+		ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
@@ -537,7 +542,8 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 	int error;
 
 #ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flag); /* 32-bit su support */
+	if (get_ksu_state() > 0)
+		ksu_handle_stat(&dfd, &filename, &flag); /* 32-bit su support */
 #endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
